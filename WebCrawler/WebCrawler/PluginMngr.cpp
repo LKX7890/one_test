c577da17f6c86a83f829dec84c2012d2d1fc7d3b
LKX7890 <30634610+LKX7890@users.noreply.github.com>
2017-09-23 01:58:31 +0800

    插件管理

--- WebCrawler/WebCrawler/PluginMngr.cpp
+++ WebCrawler/WebCrawler/PluginMngr.cpp
@@ -7,6 +7,80 @@ void PluginMngr::LoadPlugins(void)
 	typedef vector<string>::const_iterator Iter;
 	for (Iter it = g_app->m_cfg.m_loadPlugins.begin(); it != g_app->m_cfg.m_loadPlugins.end(); ++i)
 	{
+		string path = StrKit::StrCat(g_app->m_cfg.m_pluginsPath.c_str(), "/", it->c_str(), ".so", NULL);
 
+		g_app->m_log.printf(Log::LEVEL_DBG, __FILE__, __LINE__, "¼ÓÔØ\"%s\"²å¼þ", path.c_str());
+
+		void *handle = dlopen(path.c_str(), RTLD_GLOBAL | RTLD_NOW);
+		if (!handle)
+		{
+			g_app->m_log.printf(Log::LEVEL_ERR, __FILE__, __LINE__, "dlopen: %s", dlerror());
+		}
+
+		string symbol = "g_plugin";
+		symbol += *it;
+
+		Plugin *plugin = (Plugin*)dlsym(handle, symbol.c_str());
+
+		if (!plugin)
+		{
+			g_app->m_log.printf(Log::LEVEL_ERR, __FILE__, __LINE__, "dlsym: %s", dlerror());
+		}
+
+		plugin->init(g_app);
 	}
+}
+
+void PluginMngr::RegisterUrlPlugin(Plugin *plugin)
+{
+	m_urlPlugins.push_back(plugin);
+}
+
+void PluginMngr::RegisterHeaderPlugin(Plugin *plugin)
+{
+	m_headerPlugins.push_back(plugin);
+}
+
+void PluginMngr::RegisterHtmlPlugin(Plugin *plugin)
+{
+	m_htmlPlugins.push_back(plugin);
+}
+
+bool PluginMngr::UrlPluginHandle(void *arg) const
+{
+	for (vector<Plugin*>::const_iterator it = m_urlPlugins.begin(); it != m_urlPlugins.end(); ++it)
+	{
+		if (!(*it)->handler(arg))
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+bool PluginMngr::HeaderPluginHandle(void *arg) const
+{
+	for (vector<Plugin*>::const_iterator it = m_headerPlugins.begin(); it != m_headerPlugins.end(); ++it)
+	{
+		if (!(*it)->handler(arg))
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+bool PluginMngr::HtmlPluginHandle(void *arg) const
+{
+	for (vector<Plugin*>::const_iterator it = m_htmlPlugins.begin(); it != m_htmlPlugins.end(); ++it)
+	{
+		if (!(*it)->handler(arg))
+		{
+			return false;
+		}
+	}
+
+	return true;
 }
